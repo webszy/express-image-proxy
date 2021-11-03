c36fb50923805051e3b6cc2c6a51c2d19ab58673
Walter·Sun <43232635+webszy@users.noreply.github.com>
2021-11-03 17:31:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,28 +27,35 @@ app.get('/', (req, res)=>{
 app.get('/proxy', (req, res)=>{
     const download = require('download')
     const fs = require('fs')
-    const imgUrl = decodeURIComponent(req.query.url)
-    console.log('proxy image from',imgUrl)
-    res.setTimeout(10 * 1000,() => {
+    let imgUrl = decodeURIComponent(req.query.url).trim()
+    const headers={
+        'User-Agent': randomUseragent.getRandom(),
+        'Referer': '',//建议填写这两个
+        'Origin': ''
+    }
+    // 判断是否时是正确网址
+    if(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg|mp4|bmp)$/.test(imgUrl.split('?')[0]) === false){
+        return fs.createReadStream(errorPng).pipe(res.status(200).type('png'))
+    }
+    // 判断媒体类型
+    const isVideo = imgUrl.split('?')[0].toLowerCase().endsWith('mp4')
+    const type=isVideo ? 'mp4' : 'png'
+    // 超时
+    res.setTimeout(20 * 1000,() => {
+        console.log('res.setTimeout')
         fs.createReadStream(errorPng).pipe(res.status(200).type('png'))
     })
-    download(imgUrl)
+
+    console.log('proxy image from',imgUrl)
+
+    download(imgUrl,{headers})
         .then((e)=>{
-            res.status(200).type('png').send(e)
+            res.status(200).type(type).send(e)
         })
         .catch(e=>{
             console.log('download failed',e)
-            fs.createReadStream(errorPng).pipe(res.status(200).type('png'))
+            fs.createReadStream(errorPng).pipe(res.status(200).type(type))
         })
-        // .pipe(res.status(200).type('png'))
-        // .on('end', (e)=>{
-        //     console.log('download end',e)
-        // })
-        // .on('error', ()=>{
-        //     console.log('download failed')
-        //     fs.createReadStream(errorPng).pipe(res.status(200).type('png'))
-        // })
-
 })
 // catch 404
 app.use((req, res, next) => {
